b752afa2bba68302ec3f65b8a400784bb75f8def
github username <github email address>
2019-08-14 16:11:20 +0100

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Song.create(title: "this", release_year: 2009, released: true, genre: "pop", artist_name: "Me Myself & I")
+Song.create(title: "that", release_year: nil, released: false, genre: "pop", artist_name: "Me Myself & I")
+Song.create(title: "this", release_year: 2019, released: true, genre: "pop", artist_name: "Me Myself & I")
+
+
